292d1906a6531d23621163b66402989210726607
Manh Nguyen Huu <manhnh97@gmail.com>
2023-09-09 03:14:25 +0700

    code raw

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Testflight_Departures
+## You can find beta apps here!!!
+| Brands | Names/Links |
+| --- | --- |
+| Github | **[Beta Apps Are Available](https://github.com/manhnh97/CheckStatusTestflight/blob/master/Result_BetaAppsAvailable.md)** |
+| Github | **[Testflight Departures](https://github.com/manhnh97/Testflight_Departures/blob/master/Result_BetaAppsAvailable.md)** |
+| Github | **[Testflight AwesomeTestflight](https://github.com/pluwen/awesome-testflight-link)** |
+| Telegram | **[TestflightR](https://t.me/testflightR)** |
+| Telegram | **[Testflight Releases](https://t.me/TestflightReleases)** |
+| Telegram | **[Testflight Gamerv](https://t.me/tfgamerv)** |
+| Telegram | **[TestflightX](https://t.me/TestFlightX)** |
+| Telegram | **[TFGames](https://t.me/TFGames)** |
+| Telegram | **[Firstfinger](https://t.me/firstfinger)** |
+| Telegram | **[Testflightlinks](https://t.me/testflightlinks)** |
+| Telegram | **[Departures](https://t.me/departures_to)** |
+| Telegram | **[Testflight (Không gian mạng)](https://t.me/c/1823403288/32)** |
+| Telegram | **[Testflight (Thịt luộc)](https://t.me/thitluoc77/88197)** |
+| Telegram | **[App Store Deals](https://t.me/AppStoreDeals)** |
+| Threads | **[Testflightlinks](https://www.threads.net/@testflightlinks)** |
+| GSearch | **[Testflight GGsearch](https://www.google.com/search?q=site:testflight.apple.com/join/)** |
+| GSearch | **[Testflight GGsearch [Name]](https://www.google.com/search?q=XXXX++site%3Atestflight.apple.com%2Fjoin%2F&sxsrf=ALeKk00KavQhLJ4dpUUru7tjMy3PCXSK-g%3A1621736790931&ei=Vr2pYMS0OKyjjLsP0siEOA&oq=****++site%3Atestflight.apple.com%2Fjoin%2F&gs_lcp=Cgdnd3Mtd2l6EANQr58BWPSyAWDtwgFoAnAAeACAAT6IAZUDkgEBN5gBAKABAaoBB2d3cy13aXrAAQE&sclient=gws-wiz&ved=0ahUKEwjEt96q4N7wAhWsEWMBHVIkAQcQ4dUDCA0&uact=5)** |
+| GSheet | **[Testflight GGsheet](https://docs.google.com/spreadsheets/d/1Uej3AQPxRcLRXnmthUXR-7oGkNV_GsMFgCoNnuPtSwI/edit#gid=1719818428)** |
+| Website | **[Testflight Wabetainfo](https://wabetainfo.com/testflight/)** |
+| Website | **[Testflight Airport Community](https://app.airport.community/)** |
+| Website | **[Findbeta](https://findbeta.no/)** |
+| Facebook | **[Nghiện Laptop](https://www.facebook.com/groups/nghienlaptopviet)** |
+| Facebook | **[Nghiện Trick](https://www.facebook.com/groups/685853110047775)** |
+| Facebook | **[TestFlighters](https://www.facebook.com/groups/819080485228690)** |
+| Reddit | **[Testflight](https://www.reddit.com/r/TestFlight/)** |
+| X | **[Testflight Newest](https://twitter.com/search?q=url%3Atestflight.apple.com)** |
+
+## Thanks to
+**[Andy Pham](https://www.facebook.com/andypham.thitluoc)** <br/>
+**[Dimitri Romanov](https://t.me/DimiHepburn)** <br />
+**[Robin Bærheim](https://t.me/Findbeta)**
--- main.py
+++ main.py
@@ -1,36 +1,65 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+from datetime import datetime
+import re
 
+txtResult_AvailableTestflight = "Result_BetaAppsAvailable.md"
 
-url = 'https://departures.to/tags'
-r = requests.Session()
-response = r.get(url)
+with open(txtResult_AvailableTestflight, 'w', encoding='utf-8') as txtResult_AvailableTestflight_file:
+    url = 'https://departures.to/tags'
+    r = requests.Session()
+    response = r.get(url)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
-    div_with_class = soup.find("div", class_ = "grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6")
-    href_tag = []
-    for categories in div_with_class.findAll("a", href=True):
-        page=1
-        category = categories['href']
-        print(f"========== {category} ==========")
-        while True:
-            urlPage = f'{category}?page={page}'
-            response = r.get(urlPage)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            a_tags = soup.findAll('h3', {"class": "text-lg font-bold text-gray-800 dark:text-white"})
-            if a_tags:
-                for name in a_tags:
-                    if name:
-                        print(name.get_text(), end='')
-                    else:
-                        page += 1
-                        break
-                page += 1
-            else:
-                page += 1
-                break
-else:
-    print("Failed to retrieve the web page. Status code:", response.status_code)
+    nowTime = datetime.now().strftime("%d/%m/%Y %I:%M %p")
+    txtResult_AvailableTestflight_file.write(f"# Beta Apps is available\t[{nowTime}]\n")
+    txtResult_AvailableTestflight_file.write('| Image | Name | #HASHTAG |\n| --- | --- | --- | \n')
+
+    if response.status_code == 200:
+        page_number = 1
+        has_apps_links = True
+        
+        soup = BeautifulSoup(response.text, "html.parser")
+        div_with_class = soup.find("div", class_ = "grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6")
+        href_tag = []
+        for categories in div_with_class.findAll("a", href=True):
+            page=1
+            category = categories['href']
+            print(f"========== {category} ==========")
+            while True:
+                urlPage = f'{category}?page={str(page_number)}'
+                response = r.get(urlPage)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                a_tags = soup.find_all("a", href=lambda href: href and href.startswith("https://departures.to/apps/"))
+                if not a_tags:
+                    has_apps_links = False
+                    break
+                else:
+                    for a_tag in a_tags:
+                        appsOpening = a_tag.findAll('span', {"class": "w-2 h-2 inline-block bg-green-400 rounded-full"})
+                        if appsOpening:
+                            # print(a_tag["href"])
+                            # print(a_tag.find("h3", {"class": "text-lg font-bold text-gray-800 dark:text-white"}).text.strip())
+                            url_testflight = a_tag["href"]
+                            urlTestflightResponse = r.get(url_testflight)
+                            soupTestflightResponse = BeautifulSoup(urlTestflightResponse.text, 'html.parser')
+                            
+                            background_image_url = soupTestflightResponse.find('img')['src']
+                            name_testflight = soupTestflightResponse.find('h3').text
+                            
+                            button_data_attribute = soupTestflightResponse.find('button', {'wire:initial-data': True})
+                            jsondata = button_data_attribute.attrs['wire:initial-data']
+                            url_testflight = json.loads(jsondata)['serverMemo']['data']['url']
+                            
+                            name_testflight = name_testflight.replace('|', '-')
+                            hashtag_testflights = re.findall(r"\b\w+\b", name_testflight)
+                            hashtag_testflights = " ".join(["#" + hashtag.upper() for hashtag in hashtag_testflights])
+                            
+                            txtResult_AvailableTestflight_file.write(f"| <img src=\"{background_image_url}\" alt=\"{name_testflight}\" align=\"center\" width=\"40\" height=\"40\" /> | **[{name_testflight}]({url_testflight})** | {hashtag_testflights}<br />{url_testflight}\n")
+                            # print(f"| <img src=\"{background_image_url}\" alt=\"{name_testflight}\" align=\"center\" width=\"40\" height=\"40\" /> | **[{name_testflight}]({url_testflight})** | {hashtag_testflights}<br />{url_testflight}\n")
+                page_number += 1
+            page_number = 1
+    else:
+        print("Failed to retrieve the web page. Status code:", response.status_code)
 
 
