9c4e934b0aa71e5c270316c22d471d6582c4e11b
Manh Nguyen Huu <manhnh97@gmail.com>
2023-09-09 07:52:57 +0700

    Updated!!!

---
+++ Run_CheckAppsTestflight.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+title Apps beta available in Departures
+CD /D %~dp0
+
+git pull origin master
+set "Testflight_CheckStatus=main.py"
+echo Run "%Testflight_CheckStatus%" script...
+python "%Testflight_CheckStatus%"
+if %errorlevel% neq 0 (
+    goto :exit
+)
+
+echo:
+
+goto :commitGITHUB
+
+:commitGITHUB
+	set "Result_BetaAppsAvailable=Result_BetaAppsAvailable.md"
+	git add "%Result_BetaAppsAvailable%"
+	git commit -m "Updated!"
+	git push origin master
+goto :exit
+	
+:exit
+	exit /B
